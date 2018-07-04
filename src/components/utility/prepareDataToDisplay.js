f06b37c1a9e9f1ccfb888938e1b9c8331ff9cd8c
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-07-04 12:43:17 +0300

    spread added

--- src/components/utility/prepareDataToDisplay.js
+++ src/components/utility/prepareDataToDisplay.js
@@ -5,13 +5,29 @@ function cutString(string, desiredLength) {
   return `${string.slice(0, desiredLength)}...`;
 }
 
+// function prepareDataToDisplay(repos, savedRepos) {
+//   const newRepos = {};
+//   repos.forEach((repo) => {
+//     const newRepo = Object.assign({}, repo);
+//     newRepo.topics = newRepo.topics.slice(0, 4);
+//     newRepo.description = cutString(repo.description, 90);
+//     newRepo.saved = !!savedRepos[newRepo.id];
+//     newRepos[repo.id] = newRepo;
+//   });
+//   return newRepos;
+// }
+
 function prepareDataToDisplay(repos, savedRepos) {
   const newRepos = {};
   repos.forEach((repo) => {
-    const newRepo = Object.assign({}, repo);
-    newRepo.topics = newRepo.topics.slice(0, 4);
-    newRepo.description = cutString(repo.description, 90);
-    newRepo.saved = !!savedRepos[newRepo.id];
+    const newRepo = {
+      ...repo,
+      topics: repo.topics.slice(0, 4),
+      description: cutString(repo.description, 90),
+      saved: !!savedRepos[repo.id],
+    };
+    // newRepo.saved = !!savedRepos[newRepo.id];
+
     newRepos[repo.id] = newRepo;
   });
   return newRepos;
